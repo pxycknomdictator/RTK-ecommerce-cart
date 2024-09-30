e938ca308e76799fe83d415c7c32d783818a3311
Noman <pxycknomdictator@outlook.com>
2024-09-30 14:58:18 +0500

    added loader state

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <section className="container mx-auto flex items-center justify-center">
+      <div className="loader"></div>;
+    </section>
+  );
+};
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "./Card";
 import { handleGetAllProducts } from "../api/api";
-import { useEffect } from "react";
+import { Loader } from "./Loader";
 
 export const ProductsList = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     (async () => {
       const data = await handleGetAllProducts();
       setProducts(data);
+      setIsLoading(false);
     })();
   }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <ul className="container px-5 sm:px-0 mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center gap-y-6 gap-x-7">
