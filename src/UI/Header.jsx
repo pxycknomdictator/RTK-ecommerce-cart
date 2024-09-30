ba010072a7de015e62ac0ad6a93208835899b4a2
Noman <pxycknomdictator@outlook.com>
2024-09-30 14:05:40 +0500

    added header

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,3 +1,14 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="max-w-7xl mx-auto flex items-center justify-between">
+      <div>
+        <h1 className="text-3xl font-semibold">Redux Toolkit</h1>
+      </div>
+      <nav className="space-x-6 text-[1.2rem] font-semibold">
+        <a href="/">Home</a>
+        <a href="/products">Cart</a>
+        <span>Items: 0</span>
+      </nav>
+    </header>
+  );
 };
